18955e10d0d24982a10118f22980e516fd9a72d5
Renan Costa Alencar <renan.costaalencar@gmail.com>
2025-05-02 14:29:19 -0300

    Primeiro commit

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Leia a nossa documentação
           </a>
         </div>
       </main>
